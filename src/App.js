c885c70509f5c498a6c880db276e78edff4ae8cf
Nitiwat Wongsompan <Nitiwat@Nitiwats-Air.attlocal.net>
2021-12-01 09:33:13 -0600

    Adding routes for our portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
+
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
+
+import SearchGiphy from './components/searchGiphy';
+import About from './components/About'
 
 function App() {
   return (
+    <div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="about">About</Link>
+          </li>
+          <li>
+            <Link to='/search-gifs'>Search for Gifs</Link>
+          </li>
+        </ul>
+      </div>
+      <Switch>
+        {/* Route path="/"><Home></Home></Route> */}
+      <Route path='/about'>
+        <About></About>
+      </Route>
+    </Switch>
+    </Router>
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <SearchGiphy></SearchGiphy>
+    </div>
     </div>
   );
 }
